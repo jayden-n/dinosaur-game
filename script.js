a5fb092d8db90420e1bc2268ac34a4426ca65285
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2022-05-26 13:14:00 -0400

    Start/Stop Game Logic

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ import { updateGround, setupGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
+
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
 
+document.addEventListener("keydown", handleStart, { once: true });
+
 setupGround();
 let lastTime;
+let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -16,15 +24,30 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
-  updateGround(delta, 1);
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
   // console.log(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
 
-window.requestAnimationFrame(update);
-
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+}
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+}
 function setPixelToWorldScale() {
   let worldToPixelScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
